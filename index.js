62a29246ce41ac6b09ca7f392bd1ada47c3eef0a
Justin Klemm <jwklemm@gmail.com>
2019-02-05 09:03:05 -0800

    Remove sample server script

--- index.js
+++
@@ -1,8 +0,0 @@
-const http = require('http')
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'})
-  res.end('<html><body><input id="btn" type="button" value="Click me!"></body></html>\n')
-}).listen(3000, '127.0.0.1')
-
-console.log('Server running at http://127.0.0.1:3000/')
